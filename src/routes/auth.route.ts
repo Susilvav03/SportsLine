f4784392369411955c83901e6b7e069d4e1e7b76
Oomass7 <loaizarodrigueztomas@gmail.com>
2025-10-22 10:29:03 -0500

    feat(task1): add login and register with validations

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express"
+import { register, login } from "../controllers/auth.controller.ts"
+import { validateBody } from "../middlewares/validate.middleware.ts"
+import { UserRegisterSchema, UserLoginSchema } from "../schemas/user.schema.ts"
+
+const router: Router = Router()
+
+router.post("/register", validateBody(UserRegisterSchema), register)
+router.post("/login", validateBody(UserLoginSchema), login)
+
+export default router
